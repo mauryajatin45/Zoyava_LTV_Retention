c62ad4698039bd3e45d320551c84bc69d6ca7032
Jatin Maurya <mauryajatin45@gmail.com>
2026-06-26 13:05:51 +0530

    chore: remove unused product generator boilerplate

--- web/index.js
+++ web/index.js
@@ -5,7 +5,7 @@ import express from "express";
 import serveStatic from "serve-static";
 
 import shopify from "./shopify.js";
-import productCreator from "./product-creator.js";
+
 import PrivacyWebhookHandlers from "./privacy.js";
 import webhookRoutes from './routes/webhooks.js';
 import apiRoutes from './routes/api.js';
@@ -58,35 +58,7 @@ app.use('/api', apiRoutes);
 
 app.use(express.json());
 
-app.get("/api/products/count", async (_req, res) => {
-  const client = new shopify.api.clients.Graphql({
-    session: res.locals.shopify.session,
-  });
 
-  const countData = await client.request(`
-    query shopifyProductCount {
-      productsCount {
-        count
-      }
-    }
-  `);
-
-  res.status(200).send({ count: countData.data.productsCount.count });
-});
-
-app.post("/api/products", async (_req, res) => {
-  let status = 200;
-  let error = null;
-
-  try {
-    await productCreator(res.locals.shopify.session);
-  } catch (e) {
-    console.log(`Failed to process products/create: ${e.message}`);
-    status = 500;
-    error = e.message;
-  }
-  res.status(status).send({ success: status === 200, error });
-});
 
 app.use(shopify.cspHeaders());
 app.use(serveStatic(STATIC_PATH, { index: false }));
